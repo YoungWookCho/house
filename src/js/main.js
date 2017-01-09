3d96513f2f664f6d30f9b792cdcb362d6f1c0774
impact0823@naver.com <young1596>
2017-01-09 21:42:36 +0900

    slide part half complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,16 @@
 require([
 	"common",
 ], function() {
+	var adder=205/2;
+	var listResult=0;
+
+	$(".arrow-left").on("click", function() {
+		listResult+=adder;
+		$(".common-list>ul").animate({left: listResult+"px"});
+	});
+
+	$(".arrow-right").on("click", function() {
+		listResult-=adder;
+		$(".common-list>ul").animate({left: listResult+"px"});
+	});
 });
