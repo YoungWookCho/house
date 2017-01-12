2cada44220dbc49cf126c1616493d68b9176797a
impact0823@naver.com <young1596>
2017-01-12 21:11:49 +0900

    최근까지 만든것

--- src/js/main.js
+++ src/js/main.js
@@ -4,27 +4,27 @@ require([
 	var adder=260;
 	var listResult=0;
 
-	$(".arrow-left").on("click", function() {
+	$(".best-arrow-left").on("click", function() {
 		if (listResult+adder >= 0) {
 			listResult=0;
-			$(".common-list>ul").animate({left: listResult+"px"});
+			$(".best-list>ul").animate({left: listResult+"px"});
 			return;
 		}
 		listResult+=adder;
-		$(".common-list>ul").animate({left: listResult+"px"});
+		$(".best-list>ul").animate({left: listResult+"px"});
 	});
 
-	$(".arrow-right").on("click", function() {
-		var lastPosition = ($(".common-list>ul>li:last-child").position().left+250);
-		var viewLength = $(".common-list").outerWidth();
+	$(".best-arrow-right").on("click", function() {
+		var lastPosition = ($(".best-list>ul>li:last-child").position().left+250);
+		var viewLength = $(".best-list").outerWidth();
 		if ( -(lastPosition-viewLength) >= listResult-adder ) {
 			listResult=-(lastPosition-viewLength);
-			$(".common-list>ul").animate({left: listResult+"px"});
+			$(".best-list>ul").animate({left: listResult+"px"});
 
 			return;
 		}
 		listResult-=adder;
-		$(".common-list>ul").animate({left: listResult+"px"});
+		$(".best-list>ul").animate({left: listResult+"px"});
 	});
 
 	$(".select-type>ul>li").on("click", function() {
